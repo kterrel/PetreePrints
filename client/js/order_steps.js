2d5ed730236d2dc7070b3a6c09b2d7e2bb7b560b
Kelsey T <kelsey.terrel@gmail.com>
2019-04-28 20:48:56 -0400

    Condensed HTMLs

--- client/js/order_steps.js
+++ client/js/order_steps.js
@@ -1,4 +1,29 @@
-/*jquery to controll order step viewing */ 
+/* controls "How large can I print my image" popup*/
+function myPopup() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+
+/* controls image preview after uploading */
+var outImage="userImage";
+function preview_2(obj)
+{
+    if (FileReader)
+    {
+      var reader = new FileReader();
+      reader.readAsDataURL(obj.files[0]);
+      reader.onload = function (e) {
+        var image=new Image();
+        image.src=e.target.result;
+        image.onload = function () {
+            document.getElementById(outImage).src=image.src;
+        };
+        }
+    }
+else{}
+}
+
+/*jquery to control order step viewing */ 
 $('#button1').click(function()
 {
     $('#upload').show();
